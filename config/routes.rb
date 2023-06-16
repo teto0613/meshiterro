4e800a857bd4a1be0ce9caf3d6c7b16eb1bfd705
teto0613 <133244298+teto0613@users.noreply.github.com>
2023-06-16 03:12:26 +0000

    [update] meshiterro

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'lists/new'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
+  get 'homes/about' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root to: "homes#top"
+
 end
