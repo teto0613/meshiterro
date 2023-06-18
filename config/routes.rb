fb0ca7dbd2844804cfac46b8264f0b3595e2bb85
teto0613 <133244298+teto0613@users.noreply.github.com>
2023-06-18 14:52:01 +0000

    [update] meshiterro

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get 'homes/about' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  resources :post_images, only: [:new,  :create, :index, :show]
 
+  get 'lists/edit'
+  get 'homes/about' => 'homes#about', as: 'about'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
