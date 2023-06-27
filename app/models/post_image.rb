0f9ca161228cd24cb242eda022224ac7f44c59d2
teto0613 <133244298+teto0613@users.noreply.github.com>
2023-06-27 15:00:15 +0000

    [finish] meshiterro

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,6 +2,10 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  validates :shop_name, presence: true
+  validates :image, presence: true
 
   def get_image
     if image.attached?
@@ -9,6 +13,10 @@ class PostImage < ApplicationRecord
     else
       'no_image.jpg'
     end
+  end
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
   
   def get_image
